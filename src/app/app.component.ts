6051787274a779760d048ddfe9fd3fb820b2030f
Peter Springer <peterspringer829@gmail.com>
2017-01-20 12:02:45 -0700

    form connected to function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-}
+  title = 'Angular Idea Box';
+  submitForm = (e) => {
+    e.preventDefault();
+    console.log('hello')
+  };
+  text = 'hello';
+};
